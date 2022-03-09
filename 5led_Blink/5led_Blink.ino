b83bd94884b1bc5e9064fdf26ed6576eeaabf134
XiaoShuoShou <101267048+XiaoShuoShou@users.noreply.github.com>
2022-03-09 21:50:59 +0800

    i creat this :)

---
+++ 5led_Blink/5led_Blink.ino
@@ -0,0 +1,38 @@
+const int led1 = 13;
+const int led2 = 12;
+const int led3 = 11;
+const int led4 = 10;
+const int led5 = 9;
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+}
+
+void loop() {
+ 
+ digitalWrite(led1, HIGH);   
+  delay(50);                     
+  digitalWrite(led1, LOW);    
+  delay(50); 
+   digitalWrite(led2, HIGH);   
+  delay(50);                     
+  digitalWrite(led2, LOW);    
+  delay(50); 
+   digitalWrite(led3, HIGH);   
+  delay(50);                     
+  digitalWrite(led3, LOW);    
+  delay(50); 
+   digitalWrite(led4, HIGH);   
+  delay(50);                     
+  digitalWrite(led4, LOW);    
+  delay(50); 
+   digitalWrite(led5, HIGH);   
+  delay(50);                     
+  digitalWrite(led5, LOW);    
+  delay(50);                      
+}
